edb340f6dd6b5fd1c738151a02250c9bddacea2c
Marcus <marcus.friis99@gmail.com>
2026-09-24 12:15:50 +0200

    little tiger lines bg download helper

---
+++ scripts/download_shps.sh
@@ -0,0 +1,8 @@
+BASE="https://www2.census.gov/geo/tiger/TIGER2025/BG/"
+
+curl -s "$BASE" | grep -oE 'tl_2025_[0-9]+_bg\.zip' | sort -u > filelist.txt
+wc -l filelist.txt
+
+while read -r f; do
+  curl -C - -O "${BASE}${f}"
+done < filelist.txt
