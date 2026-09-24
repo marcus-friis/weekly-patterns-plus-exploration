355c0c2911e6ef65e14182061359e6925514e496
Marcus <marcus.friis99@gmail.com>
2026-09-24 15:45:19 +0200

    more sql table defs - this might be a better approach

--- sql/main.sql
+++ sql/main.sql
@@ -2,6 +2,10 @@
 -- INSTALL spatial;
 -- LOAD spatial;
 
+-- Disable preserve_insertion_order
+-- SET preserve_insertion_order = false;
+
+-- Dim block groups - from tiger shape files
 CREATE OR REPLACE TABLE block_groups AS
     SELECT * FROM ST_Read('data/tiger2025_bg/tl_2025_01_bg/tl_2025_01_bg.shp')
     UNION ALL BY NAME
@@ -115,6 +119,9 @@ CREATE OR REPLACE TABLE block_groups AS
     UNION ALL BY NAME
     SELECT * FROM ST_Read('data/tiger2025_bg/tl_2025_78_bg/tl_2025_78_bg.shp');
 
+CREATE INDEX bg_geom_idx ON block_groups USING RTREE (geom);
+
+-- Lol
 CREATE TABLE IF NOT EXISTS lol AS
     WITH t AS (
         SELECT *, ST_Point(LONGITUDE, LATITUDE) AS GEOM
@@ -126,3 +133,48 @@ CREATE TABLE IF NOT EXISTS lol AS
     JOIN block_groups bg
       ON bg.geom && t.GEOM
      AND ST_Within(t.GEOM, bg.geom);
+
+-- Dim pois table
+CREATE TABLE IF NOT EXISTS pois AS
+SELECT DISTINCT
+    ID_STORE, PERSISTENT_ID, BRAND, LOCATION_NAME,
+    STREET_ADDRESS, POI_CBG, CITY, REGION, ISO_COUNTRY_CODE,
+    TOP_CATEGORY, SUB_CATEGORY,
+    OPEN_DATE, CLOSE_DATE,
+    LONGITUDE, LATITUDE,
+    ST_Point(LONGITUDE, LATITUDE) AS GEOM
+FROM 'data/2025-weekly-patterns-plus/*.parquet';
+
+CREATE INDEX idx_pois_id_store ON pois(ID_STORE);
+
+-- Fact visits table
+CREATE TABLE IF NOT EXISTS visits AS
+SELECT
+    ID_STORE, DATE_RANGE_START, DATE_RANGE_END,
+    VISITOR_COUNTS, VISIT_COUNTS, VISITS_BY_DAY, VISITS_BY_EACH_HOUR,
+    DISTANCE_FROM_HOME, MEDIAN_DWELL,
+    VISITOR_HOME_CBGS
+    -- BUCKETED_DWELL_TIMES, DEVICE_TYPE,
+    -- RELATED_SAME_DAY_BRAND, RELATED_SAME_WEEK_BRAND,
+    -- VISITOR_COUNTRY_OF_ORIGIN, VISITOR_DAYTIME_CBGS,
+    -- VISITOR_HOME_AGGREGATION,
+FROM read_parquet('data/2025-weekly-patterns-plus/*.parquet', union_by_name=false);
+
+CREATE INDEX idx_visits_id_store ON visits(ID_STORE);
+CREATE INDEX idx_visits_date ON visits(DATE_RANGE_START);
+
+-- Fact visits by block group - unpacked VISITOR_HOME_CBGS
+CREATE TABLE IF NOT EXISTS block_group_visits AS
+SELECT
+     ID_STORE,
+     DATE_RANGE_START, DATE_RANGE_END,
+     UNNEST(map_keys(M)) AS HOME_CBG,
+     POI_CBG,
+     UNNEST(map_values(M)) AS VISITOR_COUNT
+FROM (
+     SELECT *, CAST(VISITOR_HOME_CBGS::JSON AS MAP(VARCHAR, INTEGER)) AS M
+     FROM read_parquet('data/2025-weekly-patterns-plus/*.parquet', union_by_name=false)
+);
+
+CREATE INDEX idx_bgv_poi_cbg ON block_group_visits(POI_CBG);
+CREATE INDEX idx_bgv_home_cbg ON block_group_visits(HOME_CBG);
